53903169de65fe5b0cb5f5222645954c98bd0b13
Ndayele Jonathan Awintiti <jndayele@gmail.com>
2023-08-10 19:03:57 +0000

    Add files via upload

---
+++ 2-In-1 Code Editor/index.js
@@ -0,0 +1,7 @@
+function run(){
+    let htmlCode = document.getElementById("html-code").value;
+    let cssCode = document.getElementById("css-code").value;
+    let output = document.getElementById("output");
+    
+    output.contentDocument.body.innerHTML = htmlCode+ "<style>"+cssCode+"<style>";
+}
